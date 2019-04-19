235aeba5c8242dca7808f0143c047d4d578cd802
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-04-19 13:27:51 -0500

    Token Based Authentication started Sign in & sign out working, missing Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ApplicationRecord
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/.freeze
 
+  validates :api_token, presence: true, uniqueness: true
   validates :email, presence: true, format: { with: EMAIL_REGEX },
                     uniqueness: true
 
   before_save { self.email = email.downcase }
 
   has_secure_password
+  has_secure_token :api_token
+
+  before_validation :generate_api_token
 
   has_one :headquarters
   has_many :phones
@@ -18,5 +22,14 @@ class User < ApplicationRecord
 
   def as_json(*)
     super(except: %i[password_digest created_at updated_at] )
+  end
+
+  def generate_api_token
+    return if api_token.present?
+
+    loop do
+      self.api_token = SecureRandom.hex
+      break unless User.exists? api_token: api_token
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
         get 'user/genders', to: 'users#genders'
+        post 'auth/login', to: 'users#login'
+        post 'auth/logout', to: 'users#logout'
+
         resources :users, :headquarters
     end
   end
