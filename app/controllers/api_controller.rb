4a554f8245e42eb41a4131de69a5ce08bad5e27c
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-05-01 12:50:24 -0500

    Authentication Controller & Current User

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,16 @@
 class ApiController < ApplicationController
-  before_action :set_default_format
+  before_action :set_default_format, :authenticate_user
   include ActionController::RequestForgeryProtection
+  attr_reader :current_user
 
   private
   def set_default_format
     request.format = :json
+  end
+
+  def authenticate_user
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
         get 'user/genders', to: 'users#genders'
+        post 'authenticate', to: 'authentication#authenticate'
         resources :users, :headquarters
     end
   end
